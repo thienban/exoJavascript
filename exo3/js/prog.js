8b7fa4b582fb58250d7aa695f26a873fbfa2a060
Thien Ban <thienban91@hotmail.fr>
2018-03-09 10:37:34 +0100

    exo 3

---
+++ exo3/js/prog.js
@@ -0,0 +1,27 @@
+var jour = prompt("Rentrer le jour :");
+switch(jour){
+    case "lundi":
+        console.log("Demain on est mardi");
+        break;
+    case "mardi":
+        console.log("Demain on est mercredi");
+        break;
+    case "mercredi":
+        console.log("Demain on est jeudi");
+        break;
+    case "jeudi":
+        console.log("Demain on est vendredi");
+        break;
+    case "vendredi":
+        console.log("Demain on est samedi");
+        break;
+    case "samedi":
+        console.log("Demain on est dimanche");
+        break;
+    case "dimanche":
+        console.log("Demain on est lundi");
+        break;
+    
+    
+
+}
