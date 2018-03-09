977233cc68b27a18d1af5a60e524ee619abfc6ba
ThienBan <thienban91@hotmail.fr>
2018-03-09 11:04:50 +0100

    exo4

---
+++ exo4/js/prog.js
@@ -0,0 +1,9 @@
+var num1 = Number(prompt("Rentrer le 1er nombre"));
+var num2 = Number(prompt("Rentrer le 2er nombre"));
+
+if (num1==num2){
+    console.log("les 2 numéros sont égaux");
+}else{
+    console.log("pas egaux");
+}
+
